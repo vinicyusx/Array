645f3ffdddc9ff7b1455d026f4b9b2a7330cde40
vinicyusx <vinicyus3029@gmail.com>
2022-01-25 18:20:26 -0300

    primeiro commit

---
+++ script.js
@@ -0,0 +1,23 @@
+let amigos = ["Vinicyus","Ana clara","Alex","Nycole","Sharon"]
+console.log(amigos)
+
+amigos.unshift("Aline")
+
+console.log(amigos)
+
+amigos.pop()
+
+console.log(amigos)
+
+amigos.splice(6,7, "Juliana", "Julia")
+
+console.log(amigos)
+
+amigos.shift()
+
+console.log(amigos)
+
+
+const numbers = [7,5,6,3,8,9,2,1,4]
+
+console.log(numbers.sort())
